5cb1c85349236a8a3dca02daa043ce1d8eee0bd1
Hrishabh Joshi <harishabh2019@iiitkottayam.ac.in>
2021-03-30 18:43:13 +0530

    plastic-reduce

---
+++ Day-2/Plastic Reduce/app.js
@@ -0,0 +1,43 @@
+var lat = 0
+var long = 0
+
+var x = document.getElementById("location");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+        console.log("pop")
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+
+    if (lat == 0 && long == 0) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+    }
+
+    console.log("hello")
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+
+    if (findiflong(position.coords.latitude, position.coords.longitude) > 2)
+        alert("You are too far away!!!");
+}
+
+
+function findiflong(a, b) {
+    lat1 = a
+    lon1 = b
+    lat2 = lat
+    lon2 = long
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+        (1 - c((lon2 - lon1) * p)) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a));
+}
